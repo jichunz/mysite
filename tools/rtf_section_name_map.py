1d2d431ab76ec57235e81516519c328f430e479a
James Zhu <jichunz@users.noreply.github.com>
2020-05-26 21:33:47 -0700

    Temp commit.

--- tools/rtf_section_name_map.py
+++ tools/rtf_section_name_map.py
@@ -1,15 +1,16 @@
+import csv
+
+
 def convert_section_name_map(section_name_map_file):
     in_file = open(section_name_map_file, 'r', encoding='cp1252', errors='ignore')
     lines = in_file.readlines()
 
-    out_file = open(section_name_map_file[:-4] + '.tsv', 'w')
-    for line in lines:
-        sub_strings = line.split('\t')
-        if len(sub_strings) > 4:
-            new_line = '\t'.join([sub_strings[0], sub_strings[1], sub_strings[2], sub_strings[4].strip()])
-            out_file.write(new_line + '\n')
-
-    out_file.close()
+    with open(section_name_map_file[:-4] + '.csv', 'w', newline='') as out_file:
+        csv_writer = csv.writer(out_file, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+        for line in lines:
+            sub_strings = line.split('\t')
+            if len(sub_strings) > 4:
+                csv_writer.writerow([sub_strings[0], sub_strings[1], sub_strings[2], sub_strings[4].strip()])
 
 
 if __name__ == '__main__':
