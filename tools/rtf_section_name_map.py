84382b607306753d11f961a9c495f35adced9b4b
James Zhu <jichun@gmail.com>
2020-07-01 18:50:01 -0700

    Temp commit.

--- tools/rtf_section_name_map.py
+++ tools/rtf_section_name_map.py
@@ -12,6 +12,5 @@ def convert_section_name_map(section_name_map_file):
             if len(sub_strings) > 4:
                 csv_writer.writerow([sub_strings[0], sub_strings[1], sub_strings[2], sub_strings[4]])
 
-
 if __name__ == '__main__':
     convert_section_name_map('F:\\Users\\James\\Code\\nquiry\\NQUIRY_TOOLS\\resources\\rtf\\RtfSectionName.txt')
